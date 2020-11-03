be7752e182dd64d0b44f80da61f0d0d46d058946
RaniaMahmoud <rosyblack88@yahoo.com>
2020-11-03 16:03:32 +0200

    Commit4

--- Project/Library/Library/App_Start/RouteConfig.cs
+++ Project/Library/Library/App_Start/RouteConfig.cs
@@ -13,10 +13,16 @@ namespace Library
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "MainView", action = "UserView", id = UrlParameter.Optional }
             );
         }
     }
